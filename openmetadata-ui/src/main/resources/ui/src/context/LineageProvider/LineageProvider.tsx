9a45dfee1933fa77fe2172b18c273733598ec312
Ushran Gouhar <ushran987@gmail.com>
2025-11-27 23:38:55 +0530

    Remove items of previous page and items of current page

--- openmetadata-ui/src/main/resources/ui/src/context/LineageProvider/LineageProvider.tsx
+++ openmetadata-ui/src/main/resources/ui/src/context/LineageProvider/LineageProvider.tsx
@@ -231,6 +231,13 @@ const LineageProvider = ({ children }: LineageProviderProps) => {
   const [isPlatformLineage, setIsPlatformLineage] = useState(false);
   const [dqHighlightedEdges, setDqHighlightedEdges] = useState<Set<string>>();
   const [isCreatingEdge, setIsCreatingEdge] = useState<boolean>(false);
+  const [columnsInCurrentPages, setColumnsInCurrentPages] = useState<string[]>(
+    []
+  );
+
+  const updateColumnsInCurrentPage = (columns: string[]) => {
+    setColumnsInCurrentPages(columns);
+  };
 
   // Add state for entityFqn that can be updated independently of URL params
   const [entityFqn, setEntityFqn] = useState<string>(decodedFqn);
@@ -1803,6 +1810,9 @@ const LineageProvider = ({ children }: LineageProviderProps) => {
       onPlatformViewChange,
       dqHighlightedEdges,
       useUpdateNodeInternals,
+      columnsInCurrentPages,
+      updateColumnsInCurrentPage,
+      setColumnsInCurrentPages,
     };
   }, [
     dataQualityLineage,
@@ -1863,6 +1873,9 @@ const LineageProvider = ({ children }: LineageProviderProps) => {
     onPlatformViewChange,
     dqHighlightedEdges,
     useUpdateNodeInternals,
+    columnsInCurrentPages,
+    updateColumnsInCurrentPage,
+    setColumnsInCurrentPages,
   ]);
 
   useEffect(() => {
