e349d42873fa1e0eb6f42b4f41bd36b21484274a
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2026-02-16 13:07:30 +0530

    Rename Rules Library to Test Library and update related components and translations

--- openmetadata-ui/src/main/resources/ui/src/pages/TestLibrary/TestLibraryPage.tsx
+++ openmetadata-ui/src/main/resources/ui/src/pages/TestLibrary/TestLibraryPage.tsx
@@ -16,11 +16,11 @@ import { useTranslation } from 'react-i18next';
 import PageLayoutV1 from '../../components/PageLayoutV1/PageLayoutV1';
 import TestDefinitionList from '../../components/TestLibrary/TestDefinitionList/TestDefinitionList.component';
 
-const RulesLibraryPage = () => {
+const TestLibraryPage = () => {
   const { t } = useTranslation();
 
   return (
-    <PageLayoutV1 pageTitle={t('label.rules-library')}>
+    <PageLayoutV1 pageTitle={t('label.test-library')}>
       <Row className="p-t-md" gutter={[16, 16]}>
         <Col span={24}>
           <TestDefinitionList />
@@ -30,4 +30,4 @@ const RulesLibraryPage = () => {
   );
 };
 
-export default RulesLibraryPage;
+export default TestLibraryPage;
