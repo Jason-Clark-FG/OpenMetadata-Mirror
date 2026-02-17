8a6071165fef3448a9bd3644758e5476abcbc08d
karanh37 <karanh37@gmail.com>
2026-02-17 18:32:10 +0530

    Update index.ts

--- openmetadata-ui-core-components/src/main/resources/ui/src/components/index.ts
+++ openmetadata-ui-core-components/src/main/resources/ui/src/components/index.ts
@@ -13,13 +13,45 @@
 
 // Base components
 export * from "./base/avatar/avatar";
+export * from "./base/avatar/avatar-label-group";
+export * from "./base/avatar/avatar-profile-photo";
+export * from "./base/badges/badge-groups";
 export * from "./base/badges/badges";
+export * from "./base/button-group/button-group";
+export * from "./base/buttons/app-store-buttons";
+export * from "./base/buttons/app-store-buttons-outline";
+export * from "./base/buttons/button-utility";
 export * from "./base/buttons/button";
+export * from "./base/buttons/close-button";
+export * from "./base/buttons/social-button";
+export * from "./base/buttons/social-logos";
 export * from "./base/checkbox/checkbox";
 export * from "./base/input/input";
+export * from "./base/input/hint-text";
+export * from "./base/input/input-group";
+export * from "./base/input/input-payment";
+export * from "./base/input/label";
 export * from "./base/dropdown/dropdown";
+export * from "./base/file-upload-trigger/file-upload-trigger";
 export * from "./base/select/select";
+export * from "./base/select/multi-select";
+export * from "./base/select/combobox";
+export * from "./base/select/popover";
+export * from "./base/select/select-item";
+export * from "./base/select/select-native";
 export * from "./base/form/form";
+export * from "./base/form/hook-form";
+export * from "./base/progress-indicators/progress-circles";
+export * from "./base/progress-indicators/progress-indicators";
+export * from "./base/progress-indicators/simple-circle";
+export * from "./base/radio-buttons/radio-buttons";
+export * from "./base/slider/slider";
+export * from "./base/tags/tags";
+export * from "./base/textarea/textarea";
+export * from "./base/toggle/toggle";
+export * from "./base/tooltip/tooltip";
+export * from "./base/video-player/icons";
+export * from "./base/video-player/play-button-icon";
 
 // Foundations
 export * from "./foundations/dot-icon";
