a808b655cb8e8cfc6f9f3b573fd4bb37cea43af0
karanh37 <karanh37@gmail.com>
2026-02-20 19:37:11 +0530

    fix styles

--- openmetadata-ui/src/main/resources/ui/src/styles/index.ts
+++ openmetadata-ui/src/main/resources/ui/src/styles/index.ts
@@ -25,7 +25,6 @@ import '@fontsource/inter/700.css'; // Font 700
 import '@fontsource/inter/800.css'; // Font 800
 import '@fontsource/inter/900.css'; // Font 900
 
-import '@openmetadata/ui-core-components/styles.css';
 import '@react-awesome-query-builder/antd/css/styles.css';
 import 'reactflow/dist/base.css';
 import 'reactflow/dist/style.css';
