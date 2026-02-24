be8784168f7d85c63f232e85d813fb70b4730ceb
Siddhant <siddhant@MacBook-Pro-2.local>
2026-02-24 11:38:24 +0530

    add certification to es

--- openmetadata-service/src/main/java/org/openmetadata/service/search/indexes/DomainIndex.java
+++ openmetadata-service/src/main/java/org/openmetadata/service/search/indexes/DomainIndex.java
@@ -21,7 +21,6 @@ public record DomainIndex(Domain domain) implements SearchIndex {
     ParseTags parseTags = new ParseTags(Entity.getEntityTags(Entity.DOMAIN, domain));
     doc.putAll(commonAttributes);
     doc.put("tags", parseTags.getTags());
-    doc.put("tier", parseTags.getTierTag());
     doc.put("classificationTags", parseTags.getClassificationTags());
     doc.put("glossaryTags", parseTags.getGlossaryTags());
     doc.put("upstreamLineage", SearchIndex.getLineageData(domain.getEntityReference()));
