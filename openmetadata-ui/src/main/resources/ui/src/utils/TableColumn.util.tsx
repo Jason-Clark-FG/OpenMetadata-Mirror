6a20c723f1bf1b77ea838c322cd5805fee475c06
Harsh Vador <harsh.vador@somaiya.edu>
2026-03-16 19:38:57 +0530

    fix failing unit tests

--- openmetadata-ui/src/main/resources/ui/src/utils/TableColumn.util.tsx
+++ openmetadata-ui/src/main/resources/ui/src/utils/TableColumn.util.tsx
@@ -44,6 +44,7 @@ export const ownerTableObject = <
     dataIndex: TABLE_COLUMNS_KEYS.OWNERS,
     key: TABLE_COLUMNS_KEYS.OWNERS,
     width: 280,
+    filterIcon: columnFilterIcon,
     render: (owners: EntityReference[]) => (
       <OwnerLabel
         isCompactView={false}
