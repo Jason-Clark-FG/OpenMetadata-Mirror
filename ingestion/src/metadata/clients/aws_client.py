b4b87f74758c650c0ab93461f27491285cd72be5
ulixius9 <mayursingal9@gmail.com>
2026-03-20 10:56:50 +0530

    Fix #3061: Add validation to restrict region values to valid AWS regions

--- ingestion/src/metadata/clients/aws_client.py
+++ ingestion/src/metadata/clients/aws_client.py
@@ -45,6 +45,46 @@ class AWSServices(Enum):
     LAKE_FORMATION = "lakeformation"
 
 
+VALID_AWS_REGIONS = {
+    "af-south-1",
+    "ap-east-1",
+    "ap-northeast-1",
+    "ap-northeast-2",
+    "ap-northeast-3",
+    "ap-south-1",
+    "ap-south-2",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ap-southeast-3",
+    "ap-southeast-4",
+    "ap-southeast-5",
+    "ap-southeast-7",
+    "ca-central-1",
+    "ca-west-1",
+    "eu-central-1",
+    "eu-central-2",
+    "eu-north-1",
+    "eu-south-1",
+    "eu-south-2",
+    "eu-west-1",
+    "eu-west-2",
+    "eu-west-3",
+    "il-central-1",
+    "me-central-1",
+    "me-south-1",
+    "mx-central-1",
+    "sa-east-1",
+    "us-east-1",
+    "us-east-2",
+    "us-west-1",
+    "us-west-2",
+    "us-gov-east-1",
+    "us-gov-west-1",
+    "cn-north-1",
+    "cn-northwest-1",
+}
+
+
 class AWSAssumeRoleException(Exception):
     """
     Exception class to handle assume role related issues
@@ -86,6 +126,14 @@ class AWSClient:
             if isinstance(config, AWSCredentials)
             else (AWSCredentials.model_validate(config) if config else config)
         )
+        if self.config and self.config.awsRegion:
+            region = self.config.awsRegion
+            if region not in VALID_AWS_REGIONS:
+                raise ValueError(
+                    f"Invalid AWS Region: '{region}'. "
+                    f"This looks like it might be an availability zone rather than a region. "
+                    f"Expected one of: {', '.join(sorted(VALID_AWS_REGIONS))}"
+                )
 
     @staticmethod
     def get_assume_role_config(
---
+++ ingestion/tests/unit/clients/test_aws_client.py
@@ -0,0 +1,84 @@
+#  Copyright 2025 Collate
+#  Licensed under the Collate Community License, Version 1.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  https://github.com/open-metadata/OpenMetadata/blob/main/ingestion/LICENSE
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""
+Test AWS Client region validation
+"""
+import pytest
+
+from metadata.clients.aws_client import VALID_AWS_REGIONS, AWSClient
+from metadata.generated.schema.security.credentials.awsCredentials import AWSCredentials
+
+
+class TestAWSClientRegionValidation:
+    """Validate AWS region validation in AWSClient"""
+
+    @pytest.mark.parametrize(
+        "region",
+        [
+            "us-east-1",
+            "us-west-2",
+            "eu-west-1",
+            "ap-southeast-1",
+            "us-gov-west-1",
+            "cn-north-1",
+        ],
+    )
+    def test_valid_regions_accepted(self, region):
+        config = AWSCredentials(awsRegion=region)
+        client = AWSClient(config)
+        assert client.config.awsRegion == region
+
+    @pytest.mark.parametrize(
+        "invalid_region",
+        [
+            "us-west-2b",
+            "us-east-1a",
+            "eu-west-1c",
+            "ap-southeast-1b",
+        ],
+    )
+    def test_availability_zone_rejected(self, invalid_region):
+        config = AWSCredentials(awsRegion=invalid_region)
+        with pytest.raises(ValueError, match="Invalid AWS Region"):
+            AWSClient(config)
+
+    @pytest.mark.parametrize(
+        "invalid_region",
+        [
+            "invalid-region",
+            "us-east",
+            "east-1",
+        ],
+    )
+    def test_arbitrary_string_rejected(self, invalid_region):
+        config = AWSCredentials(awsRegion=invalid_region)
+        with pytest.raises(ValueError, match="Invalid AWS Region"):
+            AWSClient(config)
+
+    def test_error_message_contains_invalid_value(self):
+        config = AWSCredentials(awsRegion="us-west-2b")
+        with pytest.raises(ValueError, match="us-west-2b"):
+            AWSClient(config)
+
+    def test_error_message_suggests_availability_zone(self):
+        config = AWSCredentials(awsRegion="us-west-2b")
+        with pytest.raises(ValueError, match="availability zone"):
+            AWSClient(config)
+
+    def test_none_config_accepted(self):
+        client = AWSClient(None)
+        assert client.config is None
+
+    def test_all_valid_regions_accepted(self):
+        for region in VALID_AWS_REGIONS:
+            config = AWSCredentials(awsRegion=region)
+            client = AWSClient(config)
+            assert client.config.awsRegion == region
