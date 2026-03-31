0fcec67b7f9e24455ca2b3f442338477f648c000
Eugenio Doñaque <eugenio.donaque@getcollate.io>
2026-03-31 18:35:54 +0200

    wip

--- bootstrap/sql/migrations/native/1.13.0/mysql/postDataMigrationSQLScript.sql
+++ bootstrap/sql/migrations/native/1.13.0/mysql/postDataMigrationSQLScript.sql
@@ -2,3 +2,19 @@ UPDATE ingestion_pipeline_entity
 SET json = JSON_REMOVE(json, '$.sourceConfig.config.computeMetrics')
 WHERE JSON_EXTRACT(json, '$.sourceConfig.config.computeMetrics') IS NOT NULL
 AND pipelineType = 'profiler';
+
+-- Add Container permissions to AutoClassificationBotPolicy for storage auto-classification support
+UPDATE policy_entity
+SET json = JSON_INSERT(
+    json,
+    '$.rules[1]',
+    JSON_OBJECT(
+        'name', 'AutoClassificationBotRule-Allow-Container',
+        'description', 'Allow adding tags and sample data to the containers',
+        'resources', JSON_ARRAY('Container'),
+        'operations', JSON_ARRAY('EditAll', 'ViewAll'),
+        'effect', 'allow'
+    )
+)
+WHERE JSON_UNQUOTE(JSON_EXTRACT(json, '$.name')) = 'AutoClassificationBotPolicy'
+  AND JSON_EXTRACT(json, '$.rules[1].name') != 'AutoClassificationBotRule-Allow-Container';
