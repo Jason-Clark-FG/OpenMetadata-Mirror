c2be1803934a20b9eab880f7c315855d53d6167c
harshsoni2024 <harshsoni2024@gmail.com>
2026-04-06 10:35:44 +0530

    pyformat fix

--- ingestion/src/metadata/ingestion/source/database/common_db_source.py
+++ ingestion/src/metadata/ingestion/source/database/common_db_source.py
@@ -664,8 +664,11 @@ class CommonDbSourceService(
             database_name = self.context.get().database
 
         referred_schema = column.get("referred_schema") or schema_name
-        referred_table_fqn = f"{self.context.get().database_service}.{database_name}.{referred_schema}.{column.get('referred_table')}"
-
+        referred_table_fqn = (
+            f"{self.context.get().database_service}."
+            f"{database_name}.{referred_schema}."
+            f"{column.get('referred_table')}"
+        )
         referred_table = self.metadata.get_by_name(entity=Table, fqn=referred_table_fqn)
         if referred_table:
             for referred_column in column.get("referred_columns"):
