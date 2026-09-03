e433cbe55bc40a03bef4189e55efce958d341abd
Rohit0301 <rj03012002@gmail.com>
2026-09-03 16:53:30 +0530

    remove the new look prop

--- openmetadata-ui/src/main/resources/ui/src/components/LineageTable/LineageTable.tsx
+++ openmetadata-ui/src/main/resources/ui/src/components/LineageTable/LineageTable.tsx
@@ -774,7 +774,6 @@ const LineageTable: FC<{ entity: SourceType }> = ({ entity }) => {
           ) : (
             <TableTags
               isReadOnly
-              newLook
               entityFqn=""
               entityType={record.entityType as EntityType}
               handleTagSelection={() => Promise.resolve()}
@@ -801,7 +800,6 @@ const LineageTable: FC<{ entity: SourceType }> = ({ entity }) => {
           ) : (
             <TableTags
               isReadOnly
-              newLook
               entityFqn=""
               entityType={record.entityType as EntityType}
               handleTagSelection={() => Promise.resolve()}
